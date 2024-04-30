f54a57307dfe7a0bb11342e17041104dc10574d1
fortinjoffrey <fortinjoffrey@gmail.com>
2024-04-30 11:06:11 +0200

    WIP

--- lets-learn-adonisjs-6/start/routes.ts
+++ lets-learn-adonisjs-6/start/routes.ts
@@ -7,6 +7,35 @@
 |
 */
 
+import { Exception } from '@adonisjs/core/exceptions'
+import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
+import fs from 'node:fs/promises'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+
+router
+  .get('/movies/:slug', async (ctx) => {
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+
+    try {
+      const movie = await fs.readFile(url, 'utf-8')
+      ctx.view.share({ movie })
+    } catch (error) {
+      throw new Exception(`Could not find a movie called ${ctx.params.slug}`, {
+        status: 404,
+        code: 'E_NOT_FOUND',
+      })
+    }
+    return ctx.view.render('pages/movies/show')
+  })
+  .as('movies.show')
+  .where('slug', router.matchers.slug())
+
+//     router.get('/movies', () => {}).as('movies.index')
+// router.get('/movie/my-awesome-movie', () => {}).as('movies.show')
+// router.get('/movies/create', () => {}).as('movies.create')
+// router.post('/movies', () => {}).as('movies.store')
+// router.get('/movie/my-awesome-movie/edit', () => {}).as('movies.edit')
+// router.put('/movie/my-awesome-movie', () => {}).as('movies.update')
+// router.delete('/movie/my-awesome-movie', () => {}).as('movies.destroy')
