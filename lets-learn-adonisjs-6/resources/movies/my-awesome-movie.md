5c7491fcf9a55d2171704bd50b703c33c9edef3d
fortinjoffrey <fortinjoffrey@gmail.com>
2024-05-29 11:16:36 +0200

    WIP

---
+++ lets-learn-adonisjs-6/resources/movies/my-awesome-movie.md
@@ -0,0 +1,6 @@
+---
+title: My awesome movie
+summary: Incididunt voluptate sunt nulla laboris dolore in.
+--- 
+
+Mollit ea elit culpa quis qui laborum pariatur fugiat nisi ad ad. Ad sint ad proident commodo cillum tempor consequat culpa ullamco. Labore proident sint magna eu aliquip dolore amet do reprehenderit enim exercitation ullamco. Eiusmod excepteur excepteur proident amet nisi amet ullamco reprehenderit.
--- lets-learn-adonisjs-6/start/routes.ts
+++ lets-learn-adonisjs-6/start/routes.ts
@@ -11,15 +11,43 @@ import { Exception } from '@adonisjs/core/exceptions'
 import app from '@adonisjs/core/services/app'
 import router from '@adonisjs/core/services/router'
 import fs from 'node:fs/promises'
+import { MarkdownFile } from '@dimerapp/markdown'
+import { toHtml } from '@dimerapp/markdown/utils'
 
-router.on('/').render('pages/home').as('home')
+router
+  .get('/', async (ctx) => {
+    const url = app.makeURL('resources/movies')
+    const files = await fs.readdir(url)
+    const movies: Record<string, any>[] = []
+
+    for (const filename of files) {
+      const movieUrl = app.makeURL(`resources/movies/${filename}`)
+      const file: string = await fs.readFile(movieUrl, 'utf8')
+      const md = new MarkdownFile(file)
+
+      await md.process()
+
+      movies.push({
+        title: md.frontmatter.title,
+        summary: md.frontmatter.summary,
+        slug: filename.replace('.md', ''),
+      })
+    }
+
+    return ctx.view.render('pages/home', { movies })
+  })
+  .as('home')
 
 router
   .get('/movies/:slug', async (ctx) => {
-    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.md`)
 
     try {
-      const movie = await fs.readFile(url, 'utf-8')
+      const file = await fs.readFile(url, 'utf-8')
+      const md = new MarkdownFile(file)
+      await md.process()
+      const movie = toHtml(md).contents
+
       ctx.view.share({ movie })
     } catch (error) {
       throw new Exception(`Could not find a movie called ${ctx.params.slug}`, {
